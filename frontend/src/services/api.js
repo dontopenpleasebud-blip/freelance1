44c51b8d35e7e717a7a38c09540c6cbebd371719
bhargav2006 <katakambhargav073@gmail.com>
2026-06-13 19:50:33 +0530

    c2

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const base = import.meta.env.BACKEND_URI || "http://localhost:5000";
+const base = import.meta.env.BACKEND_URI;
 
 const api = axios.create({
   baseURL: base,
